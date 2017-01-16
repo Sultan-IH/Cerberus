9ecd3af25bb5de83789bd494911f131102878f32
Sultan Kenjeyev <ksula0155@gmail.com>
2017-01-16 16:58:17 +0000

    finished work on face_net, start building main class and layers

---
+++ SRC/FFNetwork.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+
+class Network(object):
+    def __init__(self,layers):
+        self.sess = tf.InteractiveSession()
+        self.layers = layers
+
--- SRC/Layers/FullyConLayer.py
+++ SRC/Layers/FullyConLayer.py
@@ -1,8 +1,2 @@
 import tensorflow as tf
 import numpy as np
-
-
-class Network(object):
-    def __init__(self, layers):
-        self.sess = tf.InteractiveSession()
-        self.layers = layers
